08daf2050d380f3d43210b9e2c6d34f6d32d3cf5
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2023-10-03 19:51:38 +0100

    validatin row and seat data

--- src/reserveseatjavaonsole/ReserveSeatJavaonsole.java
+++ src/reserveseatjavaonsole/ReserveSeatJavaonsole.java
@@ -11,13 +11,16 @@ public class ReserveSeatJavaonsole
 
     public static void main(String[] args)
     {
+
+        //Declaration of variables
         char seats[][] = new char[10][10];
         boolean flag = false;
         Scanner keyboad = new Scanner(System.in);
-        int row, seat;
+        int row = 0, seat =0;
         String answer;
         String seepMapSeats;
 
+        //Fill the seating matrix
         for (int f = 0; f < seats.length; f++)
         {
             for (int c = 0; c < seats.length; c++)
@@ -26,28 +29,52 @@ public class ReserveSeatJavaonsole
             }
         }
 
+        //Welcome to the sistem
         System.out.println("----------:  WELCOME TO THE RESERVATION SEAT  SISTEM  :----------");
 
+        // Seat Reservation
         while (flag != true)
         {
-            
+
+            //Show the seatmap
             System.out.println("");
             System.out.print("Do you want to see the available seats?:  Enter Y - (for Yes) or  N -  (for not any  words): ");
             seepMapSeats = keyboad.next();
-            
+
             if (seepMapSeats.equalsIgnoreCase("Y"))
             {
                 System.out.println("");
                 DrawMap(seats);
             }
 
-            System.out.println("");
-            System.out.println("Inser Row and Seat  to Reserver.");
-            System.out.print("Row (between 0 and 9): ");
-            row = keyboad.nextInt();
+            //Made reservation
+            boolean thisOk = false;
+            while (thisOk != true)
+            {
+                System.out.println("");
+                System.out.println("Insert Row and Seat  to Reserver.");
+                System.out.print("Row (between 0 and 9): ");
+                row = keyboad.nextInt();
 
-            System.out.print("Seat (between 0 and 9): ");
-            seat = keyboad.nextInt();
+                System.out.print("Seat (between 0 and 9): ");
+                seat = keyboad.nextInt();
+
+                if (row <= 9 && row >= 0)
+                {
+                    if (seat <= 9 && seat >= 0)
+                    {
+                        thisOk = true;
+                    }
+                    else
+                    {
+                        System.out.println("Sorry. The seat number is not valid.");
+                    }
+                }
+                else
+                {
+                    System.out.println("Sorry. The row number in not valid.");
+                }
+            }
 
             if (seats[row][seat] == 'L')
             {
